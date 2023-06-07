eb81becb59532bc9757090c139c6807d6845ca8a
duancf <duancf@ziroom.com>
2023-06-07 11:14:55 +0800

    数字相关工具完善

--- JavaUtils/src/main/tool/NumberUtil.java
+++ JavaUtils/src/main/tool/NumberUtil.java
@@ -86,12 +86,69 @@ public class NumberUtil {
         return null;
     }
 
+    /**
+     * 集合做乘法
+     * @param collection
+     * @param <T>
+     * @return
+     */
+    public static <T extends Comparable> T collectionMultiply(Collection<T> collection){
+        try{
+            return (T) collection.stream().map(c -> new BigDecimal(c.toString())).reduce(BigDecimal.ZERO, BigDecimal::multiply);
+        }catch (Exception e){
+            System.out.println("【collectionMultiply】集合做乘法异常！e=" + e);
+        }
+        return null;
+    }
+
+    /**
+     * 两数做除法
+     * @param dividend  被除数
+     * @param divisor   除数
+     * @param <T>
+     * @return
+     */
+    public static <T extends Comparable> T collectionDivide(T dividend, T divisor){
+        try{
+            BigDecimal dividendBig = new BigDecimal(dividend.toString());
+            BigDecimal divisorBig = new BigDecimal(divisor.toString());
+
+            return (T) dividendBig.divide(divisorBig);
+        }catch (Exception e){
+            System.out.println("【collectionDivide】集合做除法异常！e=" + e);
+        }
+        return null;
+    }
+
+    /**
+     * 两数做减法
+     * @param num1
+     * @param num2
+     * @param <T>
+     * @return
+     */
+    public static <T extends Comparable> T collectionSubtract(T num1, T num2){
+        try{
+            BigDecimal num1Big = new BigDecimal(num1.toString());
+            BigDecimal num2Big = new BigDecimal(num2.toString());
+
+            return (T) num1Big.subtract(num2Big);
+        }catch (Exception e){
+            System.out.println("【collectionSubtract】集合做减法法异常！e=" + e);
+        }
+        return null;
+    }
+
+
     public static void main(String[] args) {
         List<Double> integerList = Arrays.asList(1.113432545, 2.0, 3.1, 4.4, 4.5);
         Set<Double> integerSet = new HashSet<Double>(integerList);
         //System.out.println(maxValue("1", "d","200"));
         //System.out.println(minValue("1", "d","200"));
         //System.out.println(collectionMinValue(integerSet));
-        System.out.println(collectionAdd(integerSet));
+        //System.out.println(collectionAdd(integerSet));
+        System.out.println(collectionMultiply(integerSet));
+        //System.out.println(collectionDivide(1.01, 2));
+        //System.out.println(collectionSubtract(1.01, 2));
     }
 }
--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 ##### 手机号、邮箱、身份证号、邮编、数字、中文字符、车牌号、微信号、qq、密码、用户名
 ### [DateUtil](https://github.com/duancf/JavaUtils/blob/master/JavaUtils/src/main/tool/DateUtil.java) --时间工具类
 ##### Date转换字符串、时间格式、时间格式转换、增加n年/月/天/分钟、获取当前年/月/天字符串、获取当前星期、获取指定日期的星期、获取两个日期之间的天数/分钟、指定具体年/月/天/小时/分钟/秒/毫秒
-### [NumberUtil]() --数字相关工具类
-#### 获取不固定参数数量最大值、最小值、获取集合的最大值、最小值、集合数字做加法
+### [NumberUtil](https://github.com/duancf/JavaUtils/blob/master/JavaUtils/src/main/tool/NumberUtil.java) --数字相关工具类
+#### 获取不固定参数数量最大值、最小值;获取集合的最大值、最小值;集合数字做加法;两数做除法、减法；
 
